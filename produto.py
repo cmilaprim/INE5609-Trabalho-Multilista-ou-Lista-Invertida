0371439c2724b210ef4fbc46747736d850157167
bastosanaa <bastosanaa11@gmail.com>
2024-12-13 20:25:30 -0300

    first commit

---
+++ produto.py
@@ -0,0 +1,14 @@
+class Produto:
+
+    def __init__(self, codigo, nome, categoria, preco, fornecedor):
+
+        self.codigo = codigo
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.fornecedor = fornecedor
+
+    def __str__(self):
+
+        return (f"Produto[codigo={self.codigo}, nome={self.nome}, "
+            f"categoria={self.categoria}, preco={self.preco:.2f}, fornecedor={self.fornecedor}]")
